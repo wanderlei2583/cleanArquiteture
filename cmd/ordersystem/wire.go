622e1ad9e28227b9be5de82671187e3ad9476a0e
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-24 16:50:14 -0300

    implementando novas funções no wire

--- cmd/ordersystem/wire.go
+++ cmd/ordersystem/wire.go
@@ -6,25 +6,32 @@ package main
 import (
 	"database/sql"
 
+	"github.com/google/wire"
+
 	"github.com/devfullcycle/20-CleanArch/internal/entity"
 	"github.com/devfullcycle/20-CleanArch/internal/event"
 	"github.com/devfullcycle/20-CleanArch/internal/infra/database"
 	"github.com/devfullcycle/20-CleanArch/internal/infra/web"
 	"github.com/devfullcycle/20-CleanArch/internal/usecase"
 	"github.com/devfullcycle/20-CleanArch/pkg/events"
-	"github.com/google/wire"
 )
 
 var setOrderRepositoryDependency = wire.NewSet(
 	database.NewOrderRepository,
-	wire.Bind(new(entity.OrderRepositoryInterface), new(*database.OrderRepository)),
+	wire.Bind(
+		new(entity.OrderRepositoryInterface),
+		new(*database.OrderRepository),
+	),
 )
 
 var setEventDispatcherDependency = wire.NewSet(
 	events.NewEventDispatcher,
 	event.NewOrderCreated,
 	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
-	wire.Bind(new(events.EventDispatcherInterface), new(*events.EventDispatcher)),
+	wire.Bind(
+		new(events.EventDispatcherInterface),
+		new(*events.EventDispatcher),
+	),
 )
 
 var setOrderCreatedEvent = wire.NewSet(
@@ -32,7 +39,10 @@ var setOrderCreatedEvent = wire.NewSet(
 	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
 )
 
-func NewCreateOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.CreateOrderUseCase {
+func NewCreateOrderUseCase(
+	db *sql.DB,
+	eventDispatcher events.EventDispatcherInterface,
+) *usecase.CreateOrderUseCase {
 	wire.Build(
 		setOrderRepositoryDependency,
 		setOrderCreatedEvent,
@@ -41,11 +51,50 @@ func NewCreateOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInt
 	return &usecase.CreateOrderUseCase{}
 }
 
-func NewWebOrderHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebOrderHandler {
+func NewWebOrderHandler(
+	db *sql.DB,
+	eventDispatcher events.EventDispatcherInterface,
+) *web.WebOrderHandler {
 	wire.Build(
 		setOrderRepositoryDependency,
 		setOrderCreatedEvent,
 		web.NewWebOrderHandler,
 	)
 	return &web.WebOrderHandler{}
+}
+
+func NewListOrdersUseCase(db *sql.DB) *usecase.ListOrdersUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		usecase.NewListOrdersUseCase,
+	)
+	return &usecase.ListOrdersUseCase{}
+}
+
+func NewWebOrderHandler(
+	db *sql.DB,
+	eventDispatcher events.EventDispatcherInterface,
+) *web.WebOrderHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		usecase.NewCreateOrderUseCase,
+		usecase.NewListOrdersUseCase,
+		web.NewWebOrderHandler,
+	)
+	return &web.WebOrderHandler{}
+}
+
+func NewOrderService(
+	db *sql.DB,
+	eventDispatcher events.EventDispatcherInterface,
+) *service.OrderService {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		usecase.NewCreateOrderUseCase,
+		usecase.NewListOrdersUseCase,
+		service.NewOrderService,
+	)
+	return &service.OrderService{}
 }
