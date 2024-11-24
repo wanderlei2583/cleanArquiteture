d78462c09355560786577df275c007264d18e696
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-24 15:55:54 -0300

    função newlistorderusecase com a execute

--- internal/usecase/list_orders.go
+++ internal/usecase/list_orders.go
@@ -12,3 +12,28 @@ type OrderListOutputDTO struct {
 type ListOrdersUseCase struct {
 	OrderRepository entity.OrderRepositoryInterface
 }
+
+func NewListOrderUseCase(
+	OrderRepository entity.OrderRepositoryInterface,
+) *ListOrdersUseCase {
+	return &ListOrdersUseCase{
+		OrderRepository: OrderRepository,
+	}
+}
+
+func (l *ListOrdersUseCase) Execute() ([]OrderListOutputDTO, error) {
+	orders, err := l.OrderRepository.List()
+	if err != nil {
+		return nil, err
+	}
+	var ordersOutput []OrderListOutputDTO
+	for _, order := range orders {
+		ordersOutput = append(ordersOutput, OrderListOutputDTO{
+			ID:         order.ID,
+			Price:      order.Price,
+			Tax:        order.Tax,
+			FinalPrice: order.FinalPrice,
+		})
+	}
+	return ordersOutput, nil
+}
