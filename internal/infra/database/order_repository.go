f11ac2c2c96c0544260c34e6fa81f3b3ac4fbb84
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-24 15:56:28 -0300

    implementando a função list

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -15,7 +15,9 @@ func NewOrderRepository(db *sql.DB) *OrderRepository {
 }
 
 func (r *OrderRepository) Save(order *entity.Order) error {
-	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
+	stmt, err := r.Db.Prepare(
+		"INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)",
+	)
 	if err != nil {
 		return err
 	}
@@ -33,4 +35,23 @@ func (r *OrderRepository) GetTotal() (int, error) {
 		return 0, err
 	}
 	return total, nil
+}
+
+func (r *OrderRepository) List() ([]entity.Order, error) {
+	rows, err := r.Db.Query("SELECT id, price, tax, final_price FROM orders")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var orders []entity.Order
+	for rows.Next() {
+		var order entity.Order
+		err := rows.Scan(&order.ID, &order.Price, &order.Tax, &order.FinalPrice)
+		if err != nil {
+			return nil, err
+		}
+		orders = append(orders, order)
+	}
+	return orders, nil
 }
