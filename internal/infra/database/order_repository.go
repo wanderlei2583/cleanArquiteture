e26bc3e9cd4bd14bccdf428b3bf4b1dbda635e4e
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-26 22:09:27 -0300

    alterando modulo

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -3,7 +3,7 @@ package database
 import (
 	"database/sql"
 
-	"github.com/devfullcycle/20-CleanArch/internal/entity"
+	"github.com/wanderlei2583/clean_arquitecture/internal/entity"
 )
 
 type OrderRepository struct {
