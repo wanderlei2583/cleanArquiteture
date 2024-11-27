0c75e82f80823bdce77199eb0d96325f00471535
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-26 22:12:48 -0300

    alterando modulo

--- internal/usecase/create_order.go
+++ internal/usecase/create_order.go
@@ -1,8 +1,8 @@
 package usecase
 
 import (
-	"github.com/devfullcycle/20-CleanArch/internal/entity"
-	"github.com/devfullcycle/20-CleanArch/pkg/events"
+	"github.com/wanderlei2583/clean_arquitecture/internal/entity"
+	"github.com/wanderlei2583/clean_arquitecture/pkg/events"
 )
 
 type OrderInputDTO struct {
